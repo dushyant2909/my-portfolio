f161cf40155436b93f4aae54623caaf74edb6b3c
Dushyant Bhutyani <dushyantb2003@gmail.com>
2024-07-16 15:09:37 +0530

    Modified about section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -18,11 +18,11 @@ const Skills = () => {
     return (
         <>
             <h2 className='font-bold text-7xl mt-40 w-full text-center md:text-6xl md:mt-32'>Skills</h2>
-            <div className='w-full h-screen  mt-5 relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark lg:h-[80vh] sm:h-[60vh] xs:h-[50vh]
+            <div className='w-full h-screen mt-5 relative flex items-center justify-center rounded-full bg-circularLight dark:bg-circularDark lg:h-[80vh] sm:h-[60vh] xs:h-[50vh]
              lg:bg-circularLightLg lg:dark:bg-circularDarkLg
              md:bg-circularLightMd md:dark:bg-circularDarkMd
              sm:bg-circularLightSm sm:dark:bg-circularDarkSm'>
-                <motion.div className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer dark:text-dark dark:bg-light lg:p-6 md:p-4 xs:text-xs xs:p-2 "
+                <motion.div className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-6 shadow-dark cursor-pointer dark:text-dark dark:bg-light lg:p-6 md:p-4 xs:text-xs xs:p-2 "
                     whileHover={{ scale: 1.05 }}
                 >
                     Web
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,6 @@ import AnimatedText from '@/components/AnimatedText'
 import Layout from '@/components/Layout'
 import React from 'react'
 import Hat from '../../public/images/hat.png'
-import whiteHat from "../../public/images/whiteHat.png"
 import Image from 'next/image'
 import Skills from '@/components/Skills'
 import Experience from '@/components/Experience'
@@ -10,19 +9,20 @@ import Experience from '@/components/Experience'
 const about = () => {
     return (
         <main className='flex w-full flex-col items-center justify-center dark:text-light'>
-            {/* <Layout className='pt-1 pb-1'> */}
-            <Layout className='pt-16'>
-                <AnimatedText text={"Passion Fuels Purpose!"} className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8 ' />
-                <div className='mt-10 flex justify-between'>
-                    <div className='w-[47%]'>
+            <Layout className='pt-28 sm:pt-24'>
+                <AnimatedText text={"Passion Fuels Purpose!"} className='mb-14 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8 ' />
+                <div className='mt-10 flex flex-wrap justify-between sm:flex-col gap-10'>
+                    <div className='w-[47%] sm:w-full'>
                         <h2 className='mb-4 text-xl font-bold uppercase text-dark/85 dark:text-light'>About Me</h2>
                         <p className='font-medium text-justify'>
                             - Hi, I'm Dushyant Bhutiyani! I'm a full-stack web developer with a strong passion for backend development.<br />
+                        </p>
+                        <p className='mt-1 sm:mt-2 text-justify font-medium'>
                             - I enjoy the challenge of building robust and scalable applications that power the functionality users see and interact with.
                         </p>
                     </div>
-                    <div className='w-[48%] pl-2'>
-                        <h2 className='mb-4 text-xl font-bold uppercase text-dark/85 dark:text-light pl-3'>Education</h2>
+                    <div className='w-[48%] pl-2 sm:pl-0 sm:w-full'>
+                        <h2 className='mb-4 text-xl font-bold uppercase text-dark/85 dark:text-light sm:pl-0 pl-3'>Education</h2>
                         <div className='flex'>
                             <span className='hidden dark:block'>-</span>
                             <Image src={Hat} className='w-8 h-8 dark:hidden' />
