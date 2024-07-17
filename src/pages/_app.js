6b57acf39dbcda2f0f98d9d3612c2a61fdaf2665
Dushyant Bhutyani <dushyantb2003@gmail.com>
2024-07-17 11:45:21 +0530

    Added favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
         <title>Dushyant Bhutiyani | Portfolio</title>
         <meta property="og:title" content="My page title" key="title" />
       </Head>
